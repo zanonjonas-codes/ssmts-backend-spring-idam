22dcaf6c0393e2f4c7c3cc9482e8215c41d70eae
Jonas Zanon <zanonjonas.codes@gmail.com>
2023-09-06 14:22:43 -0300

    initial commit

---
+++ src/main/java/com/zanonjonascodes/ssmts/idam/SsmtsBackendSpringIdamApplication.java
@@ -0,0 +1,13 @@
+package com.zanonjonascodes.ssmts.idam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmtsBackendSpringIdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmtsBackendSpringIdamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zanonjonascodes/ssmts/idam/SsmtsBackendSpringIdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanonjonascodes.ssmts.idam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmtsBackendSpringIdamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
